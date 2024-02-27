fc373b109fa2faa3145cc5e4040db9fbf35b506b
2Vladimir2 <159247721+2Vladimir2@users.noreply.github.com>
2024-02-27 10:49:35 +0200

    Create operator.php

---
+++ operator.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>   
+ 
+ <?php
+$nume = 'Иванов';
+$prenume = 'Николай';
+echo "Фамилия клиента $nume, а его имя $prenume.";
+$varsta = 30;
+print "<br />Возраст клиента  $varsta <br />"; 
+?>
+
+
+
+</body>
+</html>
